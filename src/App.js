edcdea6ff65c1808bdd4725733bf9b84b884efd5
gibran <gibranadna@gmail.com>
2020-06-30 22:45:15 +0700

    rubah

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { setStatus } from "./actions/statusAction";
+// import React from "react";
+// import { useSelector, useDispatch } from "react-redux";
+// import { setStatus } from "./actions/statusAction";
 
-function App() {
-  const dispatch = useDispatch();
-  const status = useSelector((state) => state.status);
-  const handleIn = () => {
-    dispatch(setStatus("Hello"));
-  };
-  const handleOut = () => {
-    dispatch(setStatus("GoodBye"));
-  };
-  return (
-    <div className="App">
-      <h1>{status}</h1>
-      <button onClick={handleIn} style={{ width: "10em", height: "4em" }}>
-        IN
-      </button>
-      <button onClick={handleOut} style={{ width: "10em", height: "4em" }}>
-        OUT
-      </button>
-    </div>
-  );
-}
+// function App() {
+//   const dispatch = useDispatch();
+//   const status = useSelector((state) => state.status);
+//   const handleIn = () => {
+//     dispatch(setStatus("Hello"));
+//   };
+//   const handleOut = () => {
+//     dispatch(setStatus("GoodBye"));
+//   };
+//   return (
+//     <div className="App">
+//       <h1>{status}</h1>
+//       <button onClick={handleIn} style={{ width: "10em", height: "4em" }}>
+//         IN
+//       </button>
+//       <button onClick={handleOut} style={{ width: "10em", height: "4em" }}>
+//         OUT
+//       </button>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
